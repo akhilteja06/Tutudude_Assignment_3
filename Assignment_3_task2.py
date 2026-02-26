80b8f0d5e04318160af04bacff3688df5a95a77b
Akhil_teja <99877317+akhilteja06@users.noreply.github.com>
2026-02-26 20:03:11 +0530

    Add files via upload

---
+++ Assignment_3_task2.py
@@ -0,0 +1,12 @@
+import math
+
+number = float(input("Enter a number: "))
+
+square_root = math.sqrt(number)
+natural_log = math.log(number)
+sine_value = math.sin(number)
+
+# Displaying results
+print("Square root:", square_root)
+print("Logarithm:", natural_log)
+print("Sine:", sine_value)
